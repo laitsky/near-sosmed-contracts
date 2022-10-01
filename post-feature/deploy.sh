bc6542800d160e2debc7f619bd6a3facef79c245
Vincent Diamond <vincent.diamond15@gmail.com>
2022-10-01 20:01:43 +0700

    Delete post-feature directory

--- post-feature/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-sh ./build.sh
-near delete post.dao-sosmed.testnet dao-sosmed.testnet || :
-near create-account post.dao-sosmed.testnet --masterAccount dao-sosmed.testnet --initialBalance 10
-near deploy --wasmFile res/post_feature.wasm --accountId post.dao-sosmed.testnet --initFunction new --initArgs '{}'
